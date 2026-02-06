9b5e863ff6bac253a7ccfdd275a86330b67f4681
abhijitkayal <kayalabhi04@gmail.com>
2026-02-06 22:07:20 +0530

    up

--- src/pages/GymPage.tsx
+++ src/pages/GymPage.tsx
@@ -3,12 +3,14 @@ import Activities from "../components/gym/ActivitiesAndPools";
 import GymTraining from "../components/gym/GymTraining";
 import RequiredDocs from "../components/gym/RequiredDocs";
 import GymGallery from "../components/gym/GymGallery";
+import GymCatagories from "../components/gym/GymCategories";
 
 const GymPage = () => {
   return (
     <>
       <GymHeader />
       <Activities />
+      <GymCatagories/>
       <GymTraining />
       <RequiredDocs />
       <GymGallery />
